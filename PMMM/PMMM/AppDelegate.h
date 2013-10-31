b28e6d846a34ec4376be8c92726170bcebd508d7
monmacx <monmacx@gmail.com>
2013-10-31 16:09:53 +0700

    no message

---
+++ PMMM/PMMM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PMMM
+//
+//  Created by monmacx on 10/31/2556 BE.
+//  Copyright (c) 2556 Thawatchai Pakdee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PMMM/PMMM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PMMM
+//
+//  Created by monmacx on 10/31/2556 BE.
+//  Copyright (c) 2556 Thawatchai Pakdee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
